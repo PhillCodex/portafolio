c135f83a7f337976f6931ce343fda54b237035f8
phill <avatitaofficial@outlook.com>
2025-03-11 11:39:15 -0500

    cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+function toggleTheme(){
+    const htmlElement = document.documentElement;
+    const currentTheme = htmlElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    htmlElement.setAttribute('data-theme', newTheme);
+}
+
 function btnClip(){
     var menuBtn = document.getElementById("mymenu");
 
